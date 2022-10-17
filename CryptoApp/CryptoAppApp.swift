686501bf39bc2a7f212cfa42fe6634532ae8cf38
shurup <magagaraev95@mail.ru>
2022-10-17 11:13:56 +0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Шарап Бамматов on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
